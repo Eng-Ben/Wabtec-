27703937c84212c1f4843bd40986fe9fda8f6b92
Bendik Enger <beeng0339@oslomet.no>
2026-05-05 09:00:44 +0200

    Refactor main.py for improved readability and add comments for clarity

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import time
 from plc_client import read_plc_data
 from database import init_db, save_data
 
-LOG_INTERVAL_SECONDS = 5
+LOG_INTERVAL_SECONDS = 5 # Time inbetween each data read (Seconds)
 
+# initialize the database and create the necessary tables if they don't exist
 init_db()
 
 print("Pressure logging started. Press CTRL+C to stop.")
 
 try:
+    # Continuously read data from the PLC and save it to the database at regular intervals
     while True:
         data = read_plc_data()
         save_data(data)
@@ -16,6 +18,5 @@ try:
         time.sleep(LOG_INTERVAL_SECONDS)
 
 except KeyboardInterrupt:
+    # Handle the CTRL+C signal to stop the logging gracefully
     print("Logging stopped.")
-
-    #Stop the program gracefully on CTRL+C
