a60fbd345cf822d6a71eaedcf45751c3136e59ac
Bendik Enger <beeng0339@oslomet.no>
2026-05-21 15:07:38 +0200

    fix: Correct OPC node for start_test and add missing nodes to OPC_NODES

--- config_files/real_plc.py
+++ config_files/real_plc.py
@@ -1,7 +1,7 @@
 OPC_SERVER_URL = "opc.tcp://192.168.0.100:4840"
 
 OPC_NODES = {
-    "start_test": 'ns=3;s="Tag_1"',
+    "start_test": 'ns=3;s="button_start"',
     "test_done": 'ns=3;s="finish_ligth(test)"',
 
     "step_1": 'ns=3;s="step_1"',
@@ -13,5 +13,11 @@ OPC_NODES = {
 
     "v5": 'ns=3;s="v5"',
     "v6": 'ns=3;s="v6"',
-    "v9": 'ns=3;s="v9"'
+    "v9": 'ns=3;s="v9"',
+
+    "light_dp": 'ns=3;s="light_DP"',
+    "pressure_simulation_mdp": 'ns=3;s="pressure_simulation_MDP"',
+    "pressure_simulation_mcf": 'ns=3;s="pressure_simulation_MCF"',
+    "test_bool": 'ns=3;s="testBool"',
+    "tag_1": 'ns=3;s="Tag_1"'
 }
